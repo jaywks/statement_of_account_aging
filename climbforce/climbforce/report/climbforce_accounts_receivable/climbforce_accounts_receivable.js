e3d7cd8b5aa830615bd6592d80b69d42f7588c72
jvfiel <bailabph@gmail.com>
2016-12-02 16:13:54 +0800

    custom general ledger for climbforce

---
+++ climbforce/climbforce/report/climbforce_accounts_receivable/climbforce_accounts_receivable.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Climbforce and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Climbforce Accounts Receivable"] = {
+	"filters": [
+
+	]
+}
--- climbforce/config/desktop.py
+++ climbforce/config/desktop.py
@@ -2,13 +2,23 @@
 from __future__ import unicode_literals
 from frappe import _
 
+
 def get_data():
-	return [
-		{
-			"module_name": "Climbforce",
-			"color": "orange",
-			"icon": "octicon octicon-file-directory",
-			"type": "module",
-			"label": _("Climbforce")
-		}
-	]
+    return [
+        # {
+        #	"module_name": "Climbforce",
+        #	"color": "orange",
+        #	"icon": "octicon octicon-file-directory",
+        #	"type": "module",
+        #	"label": _("Climbforce")
+        # }
+        {
+            "module_name": "Custom General Ledger",
+            "color": "#F39C12",
+            "icon": "octicon octicon-file-directory",
+            "label": _("Custom General Ledger"),
+             "link": "query-report/Climbforce General Ledger",
+            "_doctype": "GL Entry",
+            "type": "link"
+        },
+    ]
