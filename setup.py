60e2c77744e880671777c65611a967a44cdf61b6
jaywks <admin@climbforce.com>
2019-03-29 14:13:19 +0000

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
+
+with open('requirements.txt') as f:
+        install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in climbforce/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('climbforce/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
+        version = str(ast.literal_eval(_version_re.search(
+                f.read().decode('utf-8')).group(1)))
 
 setup(
-	name='climbforce',
-	version=version,
-	description='for climbforce',
-	author='Climbforce',
-	author_email='climbforce@frappeapp.com',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+        name='climbforce',
+        version=version,
+        description='Statement of Account Aging',
+        author='Climbforce FZC',
+        author_email='admin@climbforce.com',
+        packages=find_packages(),
+        zip_safe=False,
+        include_package_data=True,
+        install_requires=install_requires
 )
